6c79a230556a3d79aa9ac663790d4fa1a5ac9ac9
Raymond Kristiansen <raymond.kristiansen@uib.no>
2024-06-19 10:19:25 +0200

    feat: added support for controller bridge

--- manifests/nm/connection.pp
+++ manifests/nm/connection.pp
@@ -5,6 +5,8 @@ define network::nm::connection(
   String $connection_type = 'ethernet',
   String $interface_name = $name,
   String $autoconnect = 'true',
+  Optional[String] $controller = undef,
+  Optional[String] $port_type = 'bridge',
   Hash $ipv4 = { 'method' => 'disabled' },
   Hash $ipv6 = { 'method' => 'disabled' },
   Boolean $force_update = false,
@@ -34,9 +36,22 @@ define network::nm::connection(
       'ipv4' => $ipv4,
       'ipv6' => $ipv6
     }
+    if $controller {
+      $settings_controller = {
+        # Use master/slave-type for backward compatibility
+        'connection' => {
+          'master' => $controller,
+          'slave-type' => $port_type
+        }
+      }
+    } else {
+      $settings_controller = {}
+    }
+
+    $settings_real = deep_merge($settings, $settings_controller)
 
     $defaults = { 'path' => $config_file, 'key_val_separator' => '=', notify => Exec["reload_${uuid}"] }
-    inifile::create_ini_settings($settings, $defaults)
+    inifile::create_ini_settings($settings_real, $defaults)
 
     exec { "reload_${uuid}":
       command     => network::nm_reload_connection($uuid, 'up'),
