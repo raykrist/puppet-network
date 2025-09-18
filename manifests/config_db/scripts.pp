8a2dc61856c1860249f311b3aa3d8668263cc33f
Raymond Kristiansen <raymond@nexusweb.no>
2025-09-18 08:02:19 +0200

    feat: added script to take down all endpoint ports based on port description

---
+++ manifests/config_db/scripts.pp
@@ -0,0 +1,31 @@
+#
+# This class will add helper scripts to
+# * take down all endpoint ports
+# * take up all endpoint ports,
+# ports matching 'desc_filter' or has 'admin_status' down will not be changed
+#
+class network::config_db::scripts(
+  Array $desc_filter = ["peerlink", "downlink", "uplink", "test"],
+  Hash $interfaces_hash = $network::config_db::main::interfaces_hash,
+  Hash $default_interfaces_hash = $network::config_db::main::default_interfaces_hash,
+) {
+
+  $real_interfaces_hash = deep_merge($interfaces_hash, $default_interfaces_hash)
+  $ports = $real_interfaces_hash['PORT'].map |$port, $config| {
+    unless $desc_filter.any |$element| { $element in $config['description'] } {
+      if $config['admin_status'] == 'up' { $port }
+    }
+  }
+
+  # this will include a list of ports we want to take down and up controlled
+  $endpoint_ports = delete_undef_values($ports)
+
+  file { "down_ports.py":
+    ensure  => $ensure,
+    path    => "/usr/local/bin/down_ports.py",
+    owner   => "root",
+    mode    => '0700',
+    content => template("${module_name}/config_db/down_ports.py.erb"),
+  }
+
+}
